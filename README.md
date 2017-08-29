5d9f62d8260718bd2f76830d1776327037c2d4d9
lbias <306508354@qq.com>
2017-08-29 23:01:58 +0800

    download a page

---
+++ README.md
@@ -0,0 +1,5 @@
+# python web scraping
+
+### download a page
+![](https://github.com/lbias/python-web-scraping/blob/master/images/1.png)
+![](https://github.com/lbias/python-web-scraping/blob/master/images/2.png)
---
+++ downloading_a_page.py
@@ -0,0 +1,14 @@
+import urllib.request
+from urllib.error import URLError, HTTPError, ContentTooShortError
+
+
+def download(url):
+    print('Downloading:', url)
+    try:
+        html = urllib.request.urlopen(url).read()
+    except (URLError, HTTPError, ContentTooShortError) as e:
+        print('Download error:', e.reason)
+        html = None
+    return html
+
+print(download('http://www.0daydown.com'))
